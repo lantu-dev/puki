87c23a1f738f647900d30d3e1cfdc0a5ad32373e
liuqi <haiyan.xu.vip@gmail.comliuqi-1@bupt.edu.cn>
2021-02-11 00:34:24 +0800

    引入发送验证码页面组件，并实现初步布局
    Signed-off-by: liuqi <haiyan.xu.vip@gmail.comliuqi-1@bupt.edu.cn>

--- app/src/pages/auth/components/Phone.tsx
+++ app/src/pages/auth/components/Phone.tsx
@@ -1,9 +1,16 @@
 import { Button } from 'antd';
+import {Space, Alert, Row, Button, Typography,Input,Select,Col} from 'antd';
 import React from 'react';
 
+const {Title,Paragraph } = Typography;
+const {Option} = Select;
 interface PhoneProps {
   onConfirm: (phoneNumber: string) => void;
 }
+
+
+// BUG  居中问题
+
 
 export default function Phone(props: PhoneProps) {
   return (
@@ -11,11 +18,40 @@ export default function Phone(props: PhoneProps) {
       <div>填写手机号</div>
       <Button
         onClick={() => {
-          props.onConfirm('+8612345678912');
+          props.onConfirm('8612345678912');
         }}
       >
         确认
       </Button>
     </>
+    <div>
+      <Space direction = {"vertical"} size = {[100,0]} >
+        <Row >
+          <Col offset = {1}>
+            <Title level={3}>蓝图未来</Title>
+          </Col>
+        </Row>  
+        <Space direction = {'vertical'} size = {[10,0]}>
+          <Row>
+            <Col offset = {1}>
+              <Paragraph>手机号：</Paragraph>
+            </Col> 
+          </Row>  
+          <Row justify = {'center'}>
+            <Col>
+              <Input 
+                addonBefore={<Select defaultValue="86" className="select-before"><Option value="86">+86</Option></Select>} 
+                defaultValue=""
+                bordered = {true}/>
+            </Col>
+          </Row>
+        </Space>
+        <Row justify = {'center'}>
+          <Col>
+            <Button>发送验证码</Button> 
+          </Col>
+        </Row>  
+      </Space>
+    </div>
   );
 }
