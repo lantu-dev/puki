264c3301ae38fd103bcdd871f919d4630240b992
陈威豪 <53147830+1340908470@users.noreply.github.com>
2021-02-22 21:30:05 +0800

    刚才好像并没有格式化。。

--- app/src/pages/team/component/Filter.tsx
+++ app/src/pages/team/component/Filter.tsx
@@ -1,117 +1,111 @@
 import React from 'react';
-import {Col, Row, Select} from "antd";
-import {call} from "@/api-client";
+import { Col, Row, Select } from 'antd';
+import { call } from '@/api-client';
 
 const { Option } = Select;
 
 //比赛名称
-interface GetCompetitionNameReq {
-}
+interface GetCompetitionNameReq {}
 interface GetCompetitionNameRes {
-  result:{
-    CompetitionNames:string[]
-  }
+  result: {
+    CompetitionNames: string[];
+  };
 }
 
 //比赛类型
-interface GetCompetitionTypeReq {
-}
+interface GetCompetitionTypeReq {}
 interface GetCompetitionTypeRes {
-  result:{
-    CompetitionTypes:string[]
-  }
+  result: {
+    CompetitionTypes: string[];
+  };
 }
 
 //比赛(如互联网+，挑战杯等)
-function onChangeCompetition(value:string) {
-  console.log("onSearchCompetition"+value)
+function onChangeCompetition(value: string) {
+  console.log('onSearchCompetition' + value);
 }
-function onBlurCompetition() {
-}
-function onFocusCompetition() {
-}
+function onBlurCompetition() {}
+function onFocusCompetition() {}
 //搜索
-function onSearchCompetition(val:string) {
-  console.log(val)
+function onSearchCompetition(val: string) {
+  console.log(val);
 }
-
 
 //----------------------------------------------------------------------------------------
 
 //比赛类别（如导师科研，学生自研等）
-function onChangeCompetitionType(value:string) {
-  console.log("onChangeType"+value)
+function onChangeCompetitionType(value: string) {
+  console.log('onChangeType' + value);
 }
-function onBlurCompetitionType() {
-}
-function onFocusCompetitionType() {
-}
+function onBlurCompetitionType() {}
+function onFocusCompetitionType() {}
 //搜索
-function onSearchCompetitionType(val:string) {
-  console.log(val)
+function onSearchCompetitionType(val: string) {
+  console.log(val);
 }
 
 //----------------------------------------------------------------------------------------
 
 //招募岗位
-function onChangePosition(value:string) {
-  console.log("onChangePosition"+value)
+function onChangePosition(value: string) {
+  console.log('onChangePosition' + value);
 }
-function onBlurPosition() {
-}
-function onFocusPosition() {
-}
+function onBlurPosition() {}
+function onFocusPosition() {}
 //搜索
-function onSearchPosition(val:string) {
-  console.log(val)
+function onSearchPosition(val: string) {
+  console.log(val);
 }
-
 
 //----------------------------------------------------------------------------------------
 
-interface FilterState{
-  isCompetitionNamesFinished:boolean
-  competitionNames:string[]
-  isCompetitionTypesFinished:boolean
-  competitionTypes:string[]
+interface FilterState {
+  isCompetitionNamesFinished: boolean;
+  competitionNames: string[];
+  isCompetitionTypesFinished: boolean;
+  competitionTypes: string[];
 }
 
 export default class Filter extends React.Component {
-  state:FilterState = {
-    isCompetitionNamesFinished:false,
-    competitionNames:[],
-    isCompetitionTypesFinished:false,
-    competitionTypes:[]
-  }
-  render(){
-    let ColWidth = "auto"
+  state: FilterState = {
+    isCompetitionNamesFinished: false,
+    competitionNames: [],
+    isCompetitionTypesFinished: false,
+    competitionTypes: [],
+  };
+  render() {
+    let ColWidth = 'auto';
     //比赛名称
-    call<GetCompetitionNameReq, GetCompetitionNameRes>
-    ("CompetitionService.GetCompetitionName", {}).then(r => {
+    call<GetCompetitionNameReq, GetCompetitionNameRes>(
+      'CompetitionService.GetCompetitionName',
+      {},
+    ).then((r) => {
       if (!this.state.isCompetitionNamesFinished) {
         this.setState({
           isCompetitionNamesFinished: true,
-          competitionNames: r.result.CompetitionNames
-        })
+          competitionNames: r.result.CompetitionNames,
+        });
       }
-    })
+    });
     //比赛类别
-    call<GetCompetitionTypeReq, GetCompetitionTypeRes>
-    ("CompetitionService.GetCompetitionType", {}).then(r => {
+    call<GetCompetitionTypeReq, GetCompetitionTypeRes>(
+      'CompetitionService.GetCompetitionType',
+      {},
+    ).then((r) => {
       if (!this.state.isCompetitionTypesFinished) {
         this.setState({
           isCompetitionTypesFinished: true,
-          competitionTypes: r.result.CompetitionTypes
-        })
+          competitionTypes: r.result.CompetitionTypes,
+        });
       }
-    })
+    });
     return (
-      <Row style={{marginTop: "7px"}}>
+      <Row style={{ marginTop: '7px' }}>
         {/*按比赛/活动筛选*/}
         <Col flex={ColWidth}>
           <Select
             showSearch
-            style={{width: "95%"}}
+            style={{ width: '95%' }}
             placeholder="按比赛/活动"
             optionFilterProp="children"
             onChange={onChangeCompetition}
@@ -122,7 +116,7 @@ export default class Filter extends React.Component {
               option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
             }
           >
-            {this.state.competitionNames.map(value => (
+            {this.state.competitionNames.map((value) => (
               <Option value={value}>{value}</Option>
             ))}
           </Select>
@@ -131,7 +125,7 @@ export default class Filter extends React.Component {
         <Col flex={ColWidth}>
           <Select
             showSearch
-            style={{width: "95%"}}
+            style={{ width: '95%' }}
             placeholder="按类别"
             optionFilterProp="children"
             onChange={onChangeCompetitionType}
@@ -142,7 +136,7 @@ export default class Filter extends React.Component {
               option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
             }
           >
-            {this.state.competitionTypes.map(value => (
+            {this.state.competitionTypes.map((value) => (
               <Option value={value}>{value}</Option>
             ))}
           </Select>
@@ -151,7 +145,7 @@ export default class Filter extends React.Component {
         <Col flex={ColWidth}>
           <Select
             showSearch
-            style={{width: "95%"}}
+            style={{ width: '95%' }}
             placeholder="按岗位"
             optionFilterProp="children"
             onChange={onChangePosition}
@@ -170,6 +164,4 @@ export default class Filter extends React.Component {
       </Row>
     );
   }
-
 }
-
