a547806a0e910fc9f97ffc668e7fdf2e812f318b
陈威豪 <53147830+1340908470@users.noreply.github.com>
2021-02-23 12:15:41 +0800

    实现首屏界面项目卡片的遍历展示，下一步操作：完善卡片展示内容，实现卡片的筛选展示

--- app/src/pages/team/component/ProjectGather.tsx
+++ app/src/pages/team/component/ProjectGather.tsx
@@ -1,53 +1,48 @@
 import React from 'react';
-import { call } from '@/api-client';
-import ProjectCard from '@/pages/team/component/ProjectCard';
+import {call} from "@/api-client";
+import ProjectCard from "@/pages/team/component/ProjectCard";
 
 interface Project {
-  ProjectID: number;
-  CreateTime: string;
-  UpdateTime: string;
-  ProjectName: string;
-  ProjectDescription: string;
-  StarNum: number;
-  CommentNum: number;
+  CreateTime:string
+  UpdateTime:string
+  ProjectName:string
+  ProjectDescription:string
+  StarNum:number
+  CommentNum:number
 }
 
-interface ProjectGatherState {
-  IsFinished: boolean;
-  Projects: Project[];
+interface ProjectGatherState{
+  IsFinished:boolean
+  Projects:Project[]
 }
 
 interface GetProjectSimpleReq {}
 interface GetProjectSimpleRes {
-  ProjectSimples: Project[];
+  ProjectSimples:Project[]
 }
 
-export default class ProjectGather extends React.Component {
-  state: ProjectGatherState = {
-    IsFinished: false,
-    Projects: [],
-  };
+export default class ProjectGather extends React.Component{
+  state:ProjectGatherState = {
+    IsFinished:false,
+    Projects:[]
+  }
   render() {
-    console.log(this.state.Projects);
-    call<GetProjectSimpleReq, GetProjectSimpleRes>(
-      'ProjectService.GetProjectSimple',
-      {},
-    ).then((r) => {
-      if (!this.state.IsFinished) {
-        this.setState({
-          IsFinished: true,
-          Projects: r.ProjectSimples,
-        });
-      }
-    });
+    console.log(this.state.Projects)
+    call<GetProjectSimpleReq,GetProjectSimpleRes>("ProjectService.GetProjectSimple", {})
+      .then(r => {
+        if (!this.state.IsFinished){
+          this.setState({
+            IsFinished:true,
+            Projects:r.ProjectSimples
+          })
+        }
+
+      })
     return (
       <div>
-        {this.state.Projects.map((value) => (
-          <ProjectCard
-            ProjectID={value.ProjectID}
-            ProjectName={value.ProjectName}
-            ProjectDescribeSimple={value.ProjectDescription}
-          />
+
+        {this.state.Projects.map(value => (
+          <ProjectCard ProjectName={value.ProjectName} ProjectDescribeSimple={value.ProjectDescription}/>
         ))}
       </div>
     );
