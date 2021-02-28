f7759c52b3958bff91e0e66d76afbf99494542aa
陈威豪 <53147830+1340908470@users.noreply.github.com>
2021-02-28 17:02:11 +0800

    debug

--- app/src/api-client/index.ts
+++ app/src/api-client/index.ts
@@ -1,4 +1,4 @@
 export { call, setToken } from './client';
-
-export { auth } from './auth';
+export { default as auth } from './auth';
+export { default as events } from './events';
 export { team } from './team';
