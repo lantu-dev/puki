75349b195bd873bbe16a26dfb0b295bdb5b6bbec
Autumnal_Joy <autumnal_joy@foxmail.com>
2021-03-06 15:22:06 +0800

    feat: 小程序上增加用户模块和活动模块的webview

---
+++ miniapp/src/pages/me/index.tsx
@@ -0,0 +1,25 @@
+import { WebView } from "@tarojs/components";
+import React, { Component } from "react";
+import "./index.less";
+
+export default class Index extends Component {
+  componentWillMount() {}
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  componentDidShow() {}
+
+  componentDidHide() {}
+
+  handleMessage(e) {
+    console.log(e);
+  }
+
+  render() {
+    return (
+      <WebView src="http://127.0.0.1:8000/me" onMessage={this.handleMessage} />
+    );
+  }
+}
